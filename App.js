ff054dbc85ce2aed1b05d3dc61b82ac2462eaa00
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2022-08-30 21:22:05 -0300

    INSERIDO TOUCHABLE E SWITCH

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, Image, 
-    TextInput, Alert, ScrollView } from 'react-native';
+    TextInput, Alert, ScrollView, TouchableOpacity, Switch } from 'react-native';
 
 import estilo from './Estilo';
 
@@ -8,6 +8,18 @@ const App = () => {
   // Estado
   const [mensagem, setMensagem] = useState();
   const [nome, setNome] = useState('Adriano');
+  const [clique, setClique] = useState(false);
+  const toggleSwitch = () => setClique(previousState => !previousState);
+
+  function clicou () {
+    console.log(clique);
+    (clique) ? Alert.alert('clicou na imagem!')
+    : Alert.alert('FALSO CLIQUE');
+  }
+
+  useEffect(() => {
+    //clicou();
+  }, [clique]);
 
   return (
     <ScrollView>
@@ -15,10 +27,23 @@ const App = () => {
       <Text style={estilo.texto}>
         {nome}
       </Text>
-      <Image 
-        style={estilo.logo}
-        source={require('./assets/icon.png')}
+
+      <TouchableOpacity 
+        onPress={setClique}>
+        <Image 
+          style={estilo.logo}
+          source={require('./assets/icon.png')}
+        />
+      </TouchableOpacity>
+
+      <Switch
+        trackColor={{false: "#f00", true: "#000"}}
+        thumbColor={clique ? "#0ef" : "#0f0"}
+        ios_backgroundColor="#ccc"
+        onValueChange={toggleSwitch}
+        value={clique}
       />
+      
       <Image
         style={estilo.logo}
         source={{
